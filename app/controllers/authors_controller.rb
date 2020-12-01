ae1a2c0d36dae7339b84a71f0415a4af9e6da2b5
RichaDubey <richayuvasoft251@gmail.com>
2020-12-01 11:02:48 +0530

    gargi_Changes

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -63,6 +63,10 @@ class AuthorsController < ApplicationController
   def master_cahnges
     
   end
+
+  def merge_changes
+    
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_author
